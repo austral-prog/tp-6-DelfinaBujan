b3af976d6988ef5bc23ca329e3d4488d60f77742
DelfinaBujan <dbujan@mail.austral.edu.ar>
2025-05-11 11:18:31 -0300

    Update lists.py

--- lists.py
+++ lists.py
@@ -1,6 +1,4 @@
-
-
-def remove_elements(list_to_remove_elements):
+def remove_elements (list_to_remove_elements):
 	len_lista= len(list_to_remove_elements)
 	if len_lista>= 6 :
 		del list_to_remove_elements [5]
@@ -16,22 +14,18 @@ def remove_elements(list_to_remove_elements):
 		return list_to_remove_elements	
 	else:
 		return []
-
-
-def add_elements(list_to_add_elements):
-   	list_to_add_elements.append("Yellow")
+def add_elements (list_to_add_elements):
+	list_to_add_elements.append("Yellow")
 	list_to_add_elements.insert(0,"Pink")		
 	return list_to_add_elements
-
-def is_empty(list_to_check):
-    	if list_to_check == []:
+def is_empty (list_to_check):
+	if list_to_check == []:
 		return True 	
 	else:
 		return False
 
-
-def check_lists(list_to_compare1, list_to_compare2):
-    	lista1= list_to_compare1
+def check_lists(list_to_compare1,list_to_compare2):
+	lista1= list_to_compare1
 	lista2= list_to_compare2
 	if len (lista1)	>= 3 and len (lista2) >=3:
 		if lista1[2]==lista2[2]	:
@@ -42,9 +36,8 @@ def check_lists(list_to_compare1, list_to_compare2):
 		valor=False
 	return valor
 
-
 def list_of_lists(list_of_lists_to_modify):
-    	sub1= list_of_lists_to_modify[0]
+	sub1= list_of_lists_to_modify[0]
 	sub2= list_of_lists_to_modify [1]	
 	sub3= list_of_lists_to_modify [2]	
 	if len(sub1)>0:
@@ -58,4 +51,4 @@ def list_of_lists(list_of_lists_to_modify):
 	if len (sub3) > 0:
 		sub3=sub3[-2:]	
 	todo=[sub1, sub2, sub3]
-	return todo
+	return todo			
